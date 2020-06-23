2d8d1bb7da95fadf5b7ba504d935289c2b40fbf7
Ruolin QU <quruolin95@gamil.com>
2020-06-23 11:34:29 +0800

    parcel install

---
+++ src/app.js
@@ -0,0 +1,11 @@
+// <script src="./node_modules/vue/dist/vue.js"></script>
+//     <script src="./button.js"></script>
+import Vue from 'vue'
+import Button from './button'
+
+Vue.component('g-button',Button)
+
+new Vue({
+    el: '#app',
+
+})
